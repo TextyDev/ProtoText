b289bcbd20def0ea31d60e78f2249cbbde119b65
TextyDev <oddthoughtblog@gmail.com>
2013-08-19 19:53:26 -0700

    Update BIOTCHES!!!

--- Text Adventure Testing/main.cpp
+++ Text Adventure Testing/main.cpp
@@ -10,8 +10,8 @@ string rN = "This is a dead end.\nThe doorway seals shut.\nThere is no way out.\
 
 /*Room Functions go here*/
 
-int room1();
-int roomN();
+void room1();
+void roomN();
 
 /*Store inputs here.*/
 
@@ -19,27 +19,12 @@ string input;
 
 int main()
 {
-    int room1();
-
-    /*cout << r1;
-
-    cin >> input;
-
-    if (input=="north" || "NORTH" || "n" || "N")
-        {
-            cout << rN;
-        }
-    else;
-        {
-            cout << "I didn't get that." << endl;
-        }*/
+    room1();
 
     return 0;
 }
 
-int room1(){
+void room1(){
     cout << r1;
     cin >> input;
-
-    return 0;
 }
